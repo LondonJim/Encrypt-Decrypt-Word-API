5b2f6a3dfadbdb7a5421ea713ed8880dd083b743
James Sutherland <jrsutherland78@googlemail.com>
2019-05-14 23:51:17 +0100

    tidy up line indentations

--- src/main/java/wordcrypt/EncryptWord.java
+++ src/main/java/wordcrypt/EncryptWord.java
@@ -6,26 +6,26 @@ import java.util.Base64;
 import java.util.Base64.Encoder;
 
 public class EncryptWord {
-  private String encryptedWord;
+    private String encryptedWord;
 
-  public String getEncryptedWord() {
-      return encryptedWord;
-  }
-
-  public String encrypt(String strClearText,String strKey) throws Exception{
-    byte[] encryptedBytes;
-    try {
-        SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"AES");
-        Cipher cipher=Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
-        encryptedBytes=cipher.doFinal(strClearText.getBytes());
-    } catch (Exception e) {
-        e.printStackTrace();
-        throw new Exception(e);
+    public String getEncryptedWord() {
+        return encryptedWord;
     }
-    Encoder encoder = Base64.getEncoder();
-    encryptedWord = encoder.encodeToString(encryptedBytes);
-    return encryptedWord;
-  }
+
+    public String encrypt(String strClearText,String strKey) throws Exception{
+      byte[] encryptedBytes;
+      try {
+          SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"AES");
+          Cipher cipher = Cipher.getInstance("AES");
+          cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
+          encryptedBytes = cipher.doFinal(strClearText.getBytes());
+      } catch (Exception e) {
+          e.printStackTrace();
+          throw new Exception(e);
+      }
+      Encoder encoder = Base64.getEncoder();
+      encryptedWord = encoder.encodeToString(encryptedBytes);
+      return encryptedWord;
+    }
 
 }
